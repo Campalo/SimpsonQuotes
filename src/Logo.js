28122cc1560d2dc70e666a1c217b5e05539a6f8c
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-17 13:43:44 +0200

    add logo.js

---
+++ src/Logo.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import "./App.css";
+import logo from "./logo.svg";
+
+class MyLogo extends Component {
+  // Thanks to those following lines, 
+  // the logo rendering now depends on the state, 
+  // and not on the props any more
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: this.props.working
+    };
+  }
+  // There we can modify the state of the logo thanks to setState
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+    console.log('Button Clicked');
+  };
+  render() {
+    const homerDoing = this.state.working ? 'logo whish to have a break. Click!' : 'Logo is eager to work. Click!';
+    const logoDoing = this.state.working ? 'App-logo' : 'App-logo-stop';
+    return (
+      <div className="My-column">  
+        <img src={logo} className={logoDoing}/>
+        <button className={homerDoing} onClick={this.handleClick.bind(this)} className="My-button">
+            {homerDoing.toUpperCase()}
+        </button> 
+      </div>
+    );
+  }
+}
+
+export default MyLogo;
