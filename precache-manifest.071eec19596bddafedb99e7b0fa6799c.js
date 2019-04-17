6290874e6903ea57fb2bb6aa88f98aa259ff68c7
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-17 13:51:08 +0200

    Updates

---
+++ precache-manifest.071eec19596bddafedb99e7b0fa6799c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91a18165885208016fb7",
+    "url": "/Campalo/SimpsonQuotes.git/static/css/main.4c3e8515.chunk.css"
+  },
+  {
+    "revision": "91a18165885208016fb7",
+    "url": "/Campalo/SimpsonQuotes.git/static/js/main.34b23acc.chunk.js"
+  },
+  {
+    "revision": "d0458252cd8b6f72c5a1",
+    "url": "/Campalo/SimpsonQuotes.git/static/js/runtime~main.3d9a4bb4.js"
+  },
+  {
+    "revision": "51f8a1f6cd4a8c320a8f",
+    "url": "/Campalo/SimpsonQuotes.git/static/js/2.5021ba90.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Campalo/SimpsonQuotes.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "011aa40a9bd7c07fda33343d76807f6c",
+    "url": "/Campalo/SimpsonQuotes.git/index.html"
+  }
+];
